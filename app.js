d5555c3ee020b01c949c89427869109595344265
Nanaya Miki <nanayamiki@gmail.com>
2022-06-01 18:31:38 -0700

    mod DBURL

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use("/api/v1/tasks", taskRoute)
 //connecting with database
 const start = async()=>{
     try{
-        await connectDB(process.env.MONGO_URL)   
+        await connectDB(process.env.MONGO_HEROKU_URL || process.env.MONGO_URL)   
         app.listen(process.env.PORT || PORT, console.log("server is running...!")) 
     } catch (err) {
         console.log(err)
